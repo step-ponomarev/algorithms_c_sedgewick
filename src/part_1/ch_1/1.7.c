450ede821f43b8664c9daa673c3ab139f3dc07d2
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2025-05-13 17:41:18 +0300

    1.7

---
+++ src/part_1/ch_1/1.7.c
@@ -0,0 +1,42 @@
+#include "../../lib/graph_link.h"
+#include <stdio.h>
+
+// use src/lib/graph_link_quck_union
+
+static int id[MAX_VAL];
+static int sz[MAX_VAL];
+
+int main(const int argc, char *argv[]) {
+  for (int i = 0; i < MAX_VAL; i++) {
+    id[i] = i;
+    sz[i] = 1;
+  }
+
+  int p, q;
+  while (scanf("%d %d\\n", &p, &q) == 2) {
+    if (!is_valid(p) || !is_valid(q)) {
+      fprintf(stderr, "Values must be < %d, p: %d, q: %d\n", MAX_VAL, p, q);
+      return -1;
+    }
+
+    // one root -> one union
+    const int pV = find_root(id, p);
+    const int pQ = find_root(id, q);
+    if (pV == pQ) {
+      continue;
+    }
+
+    if (sz[pV] > sz[pQ]) { // присоеденяем к большему меньший
+      merge_unions(id, pQ, pV);
+      sz[pV] += sz[pQ];
+    } else {
+      merge_unions(id, pV, pQ);
+      sz[pQ] += sz[pV];
+    }
+
+    merge_unions(id, pV, pQ);
+    printf("%10d-%d, id[i]x%d\n", p, q, MAX_VAL);
+  }
+
+  return 0;
+}
