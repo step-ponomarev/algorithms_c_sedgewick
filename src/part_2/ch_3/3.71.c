ad47889452037586c880beac40f457dcce97298e
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2025-05-20 00:21:28 +0300

    3.71

---
+++ src/part_2/ch_3/3.71.c
@@ -0,0 +1,43 @@
+#include "../../lib/malloc_utils.h"
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(const int argc, const char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Expected int N count of pairs\n");
+    return -1;
+  }
+
+  const int N = atoi(argv[1]);
+  if (N <= 0) {
+    fprintf(stderr, "N must be in range (0, %d]\n", INT_MAX);
+    return -1;
+  }
+
+  int **graph = (int **)malloc2d(N, N, sizeof(int));
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      graph[i][j] = 0;
+    }
+  }
+
+  int p, q;
+  while (scanf("%d %d", &p, &q) == 2) {
+    graph[p][q] = 1;
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (graph[i][j] == 0) {
+        continue;
+      }
+
+      printf("%d->%d\n", i, j);
+    }
+  }
+
+  free2d((void **)graph, N);
+
+  return 0;
+}
