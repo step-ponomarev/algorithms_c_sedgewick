7f90a7fd2faf53544fe131ded614cbb4ca08cbd5
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2025-05-30 19:43:43 +0300

    4.9

---
+++ src/part_3/ch_4/4.9.c
@@ -0,0 +1,41 @@
+#include "../../collections/stack.h"
+#include <stddef.h>
+#include <stdio.h>
+
+int main(const int argc, const char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Expected expression\n");
+    return -1;
+  }
+
+  Stack *stack = stack_create(sizeof(char));
+
+  const char *expression = argv[1];
+  printf("%s\n", expression);
+  size_t p = 0;
+  for (size_t p = 0; expression[p] != '\0'; p++) {
+    if (expression[p] == '(') {
+      continue;
+    }
+
+    if (expression[p] >= '0' && expression[p] <= '9') {
+      printf("%c ", expression[p]);
+      continue;
+    }
+
+    if (expression[p] == ')') {
+      char op;
+      stack_pop(stack, &op);
+
+      printf("%c ", op);
+      continue;
+    }
+
+    stack_push(stack, &expression[p]);
+  }
+  printf("\n");
+
+  stack_destroy(stack);
+
+  return 0;
+}
